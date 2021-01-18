1ee024f0670eed05a03b777c9cbe985761bc61dd
sujanbudathoki <sujanbudathoki24@gmail.com>
2021-01-18 20:25:34 +0545

    /*Game Reset Function()*/

--- Trexrunner/Trexrunner/Form1.cs
+++ Trexrunner/Trexrunner/Form1.cs
@@ -12,6 +12,17 @@ namespace Trexrunner
 {
     public partial class Form1 : Form
     {
+        bool Jumping = false;
+        int force = 12;
+        int jumpSpeed;
+        int score = 0;
+        int obstacleSpeed = 12;
+        Random rand = new Random();
+        int position;
+        bool isGameOver = false;
+      
+     
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +30,25 @@ namespace Trexrunner
 
         private void GameReset()
         {
+            obstacleSpeed = 12;
+            jumpSpeed = 0;
+            Jumping = false;
+            score = 0;
+            isGameOver = false;
+            Trex.Top = 278;
+
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "Obstacle")
+                {
+                    int position = this.ClientSize.Width + rand.Next(500, 800) + (x.Width * 10);
+                    x.Left = position;
+                }
+
+
+
+            }
+            gameTimer.Start();
 
         }
 
